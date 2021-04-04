43a2101d754cc6115ae9e7afe24234229e293ffc
Latifa Hammani <81419806+hammanilatifa@users.noreply.github.com>
2021-04-04 14:29:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
-mkdir -p ~/.streamlit
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"latifasa1998@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
